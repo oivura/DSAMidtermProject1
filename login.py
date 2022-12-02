b7dc677fd9b6bd19ad6d0a4b2da5bd8d9152a819
Ryykk <114269526+Ryykk@users.noreply.github.com>
2022-12-03 07:41:15 +0800

    Update login.py

---
+++ login.py
@@ -0,0 +1,24 @@
+import pwinput
+import csv
+
+def login():
+    print('Login Form')
+    username = input('Username: ')
+    password = pwinput.pwinput(prompt='Password: ', mask='*')
+    with open('database.csv') as csv_file:
+        reader = csv.reader(csv_file)
+        is_success = False
+        for row in reader:
+            if len(row) > 1:
+                if row[0] == username and row[1] == password:
+                    is_success = True
+        
+        if is_success:
+            print('Login Success')
+        else:
+            print('Login Failed')
+        global is_exit
+        global main_menu_input
+        is_exit = True
+        main_menu_input = ''
+        # time.sleep(10)
